8699c6916def9f42abc50df76a88255fc6cb61b2
Anton Minchenko <anton.minchenko@sigma.software>
2021-12-07 19:23:20 +0200

    Create icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PositionsListComponent } from './components/positions-list/positions-list/positions-list.component';
+import { IconComponent } from './shared/components/icon/icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PositionsListComponent
+    PositionsListComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/icon/icon/icon.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  template: '<img src="assets/logo.png">'
+})
+export class IconComponent { }
