c1c77e5c00fd67e509ba5b6ac97f0a7bebc8e746
Anton Minchenko <anton.minchenko@sigma.software>
2021-12-07 14:24:40 +0200

    Create domain

---
+++ src/domain/asset.ts
@@ -0,0 +1,8 @@
+export class Asset {
+    constructor(
+        public readonly id: number,
+        public readonly symbol: string
+    ) {
+        
+    }
+}
---
+++ src/domain/exchange.ts
@@ -0,0 +1,8 @@
+export class Exchange {
+    constructor(
+        public id: number,
+        public name: string
+    ) {
+        
+    }
+}
