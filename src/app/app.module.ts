a57c9fb04c6bf548039088a938178f3a45243e79
Anton Minchenko <anton.minchenko@sigma.software>
2021-12-07 19:27:30 +0200

    Remove unnecessary folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PositionsListComponent } from './components/positions-list/positions-list/positions-list.component';
-import { IconComponent } from './shared/components/icon/icon/icon.component';
+import { PositionsListComponent } from './components/positions-list/positions-list.component';
+import { IconComponent } from './shared/components/icon/icon.component';
 
 @NgModule({
   declarations: [
