c2c6d75957fd3e2b7d19f98f0cbc5eb977eaedd4
Anton Minchenko <anton.minchenko@sigma.software>
2021-12-07 19:30:12 +0200

    Add styles to icon

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
-  template: '<img src="assets/logo.png">'
+  template: '<img src="assets/logo.png">',
+  styles: ['img { width: 64px; height: 64px; }']
 })
 export class IconComponent { }
