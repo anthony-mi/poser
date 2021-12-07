3862e6824a6a301c40d03d72faf19789de24dca1
Anton Minchenko <anton.minchenko@sigma.software>
2021-12-07 17:40:03 +0200

    Create positions-list component and update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PositionsListComponent } from './components/positions-list/positions-list/positions-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/positions-list/positions-list/positions-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Position } from 'src/app/domain/position';
+import { PositionsService } from 'src/app/services/positions.service';
+
+@Component({
+  selector: 'app-positions-list',
+  templateUrl: './positions-list.component.html',
+  styleUrls: ['./positions-list.component.css']
+})
+export class PositionsListComponent implements OnInit {
+  private _positions: Array<Position> = [];
+
+  get positions() {
+    return this._positions;
+  }
+
+  constructor(private positionsService: PositionsService) { }
+
+  ngOnInit(): void {
+    this.positionsService
+      .getPositions()
+      .subscribe(positions => this._positions = positions);
+  }
+
+  requestEdit(position: Position) {
+    throw new Error('Method not implemented');
+  }
+
+  requestRemove(position: Position) {
+    throw new Error('Method not implemented');
+  }
+
+}
